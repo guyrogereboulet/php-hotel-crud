5107f5da82a014798425176b5b36fd90810ece47
Guy Roger Eboulet <eboulet87@gmail.com>
2020-03-04 17:47:03 +0100

    Print ID and FLOOR

--- database.php
+++ database.php
@@ -11,10 +11,26 @@
 
  $sql = "SELECT * FROM `stanze`";
  $result = $conn->query($sql);
- var_dump($result);
+
+ if ($result && $result->num_rows > 0) {
+
+   $rooms = [];
+   while ($row = $result->fetch_assoc()) {
+      // var_dump($row);
+      // echo " ID: " .$row["id"]." - Floor: " .$row["floor"];
+      $rooms[] = $row;
+
+   }
+ }
+ elseif ($result) {
+   echo "No results";
+ }
+ else {
+   echo "Query error";
+ }
 
 
 
-
+$conn->close();
 
 ?>
